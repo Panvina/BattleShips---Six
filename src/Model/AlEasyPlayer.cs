f0f816d265ceb35316691f12bb583ffcf5c6f049
Panvina <42162390+Panvina@users.noreply.github.com>
2018-10-01 15:58:52 +1000

    fixed_difficulity

--- src/Model/AlEasyPlayer.cs
+++ src/Model/AlEasyPlayer.cs
@@ -2,83 +2,59 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-//using System.Data;
 using System.Diagnostics;
-
+using SwinGameSDK;
 /// <summary>
-/// ''' EasyAIPlayer is a type of AIPlayer. All shot that are taken are random
-/// ''' shot generated with the GenerateCoords
-/// ''' </summary>
+/// The AIEasyPlayer is a type of AIPlayer where it will try to hit enemy's ships randomly
+/// </summary>
 
-namespace Battleships
-{
-    public class AIEasyPlayer : AIPlayer
-    {
-        public AIEasyPlayer(BattleShipsGame controller) : base(controller)
-        {
+namespace Battleships{
+	public class AIEasyPlayer : AIPlayer
+	{
+		
+		public AIEasyPlayer(BattleShipsGame controller) : base(controller)
+		{
+		}
+
+		/// <summary>
+		/// GenerateCoordinates should generate random shooting coordinates
+		/// only when it has not found a ship, or has destroyed a ship and 
+		/// needs new shooting coordinates
+		/// </summary>
+		/// <param name="row">the generated row</param>
+		/// <param name="column">the generated column</param>
+		protected override void GenerateCoords(ref int row, ref int column)
+		{
+			do {
+				// AI random row and column to shot
+				SearchCoords(ref row, ref column);
+			} while ((row < 0 || column < 0 || row >= EnemyGrid.Height || column >= EnemyGrid.Width || EnemyGrid[row, column] != TileView.Sea));
+            //while inside the grid and not a sea tile do the search
         }
-        private Location _nextLocation;
-        /// <summary>
-        ///     ''' GenerateCoords generates shots at random location.
-        ///     ''' </summary>
-        ///     ''' <param name="row">the row it generates</param>
-        ///     ''' <param name="column">the column it generates</param>
-        protected override void GenerateCoords(ref int row, ref int column)
-        {
-            do
-            {
-                if (_nextLocation != null)
-                {
-                    row = _nextLocation.Row;
-                    column = _nextLocation.Column;
-                    _nextLocation = null;
-                }
-                else
-                {
-                    row = Player._Random.Next(0, EnemyGrid.Height);
-                    column = _Random.Next(0, EnemyGrid.Width);
-                }
-            }
-            while ((row < 0 || column < 0 || row >= EnemyGrid.Height || column >= EnemyGrid.Width || EnemyGrid[row, column] != TileView.Sea));
-        }
+			
+		/// <summary>
+		/// SearchCoords will randomly generate shots within the grid as long as its not hit that tile already
+		/// </summary>
+		/// <param name="row">the generated row</param>
+		/// <param name="column">the generated column</param>
+		private void SearchCoords(ref int row, ref int column)
+		{
+			row = _Random.Next(0, EnemyGrid.Height);
+			column = _Random.Next(0, EnemyGrid.Width);
+		}
 
-        /// <summary>
-        ///     ''' ProcessShot does nothing with the easy AIPlayer because this player
-        ///     ''' doesn't know anything beside generating random coordinates
-        ///     ''' </summary>
-        ///     ''' <param name="row">row it shot at</param>
-        ///     ''' <param name="col">the col it shot at</param>
-        ///     ''' <param name="result">the result from the last shot</param>
-        protected override void ProcessShot(int row, int col, AttackResult result)
-        {
-            if (result.Value == ResultOfAttack.Hit)
-            {
-                switch (_Random.Next(4))
-                {
-                    case 0:
-                        _nextLocation = new Location(row + 1, col);
-                        break;
-                    case 1:
-                        _nextLocation = new Location(row - 1, col);
-                        break;
-                    case 2:
-                        _nextLocation = new Location(row, col + 1);
-                        break;
-                    case 3:
-                        _nextLocation = new Location(row, col - 1);
-                        break;
-                }
+		/// <summary>
+		/// ProcessShot will be called uppon when a ship is found.
+		/// </summary>
+		/// <param name="row">the row it needs to process</param>
+		/// <param name="col">the column it needs to process</param>
+		/// <param name="result">the result og the last shot (should be hit)</param>
 
-                if (_nextLocation.Row < 0)
-                    _nextLocation.Row = 0;
-                else if (_nextLocation.Row >= PlayerGrid.Height)
-                    _nextLocation.Row = PlayerGrid.Height - 1;
-
-                if (_nextLocation.Column < 0)
-                    _nextLocation.Column = 0;
-                else if (_nextLocation.Column >= PlayerGrid.Width)
-                    _nextLocation.Column = PlayerGrid.Width - 1;
-            }
-        }
-    }
+		protected override void ProcessShot(int row, int col, AttackResult result)
+		{
+			if (result.Value == ResultOfAttack.ShotAlready) {
+				throw new ApplicationException("Error in AI");
+			}
+		}
+	}
 }
